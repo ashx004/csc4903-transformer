c1a9f1cc2b60fd3e56bc00fd880db63c4eb340cc
ashx004 <ashx004@gmail.com>
2026-07-16 16:15:33 -0500

    Corrected documentation and mask flag, and added context parameter for decoder multihead use

--- attention.py
+++ attention.py
@@ -2,14 +2,14 @@ import torch
 import torch.nn as nn
 import math
 
-# is_encoded tells us what layer we are in, True -> decoder layer, False -> encoder layer
-def scaled_dot_product_attention(Q, K, V, is_encoded=True):
+# is_masked what type of attention we are doing, True -> masked, False -> normal MH attention
+def scaled_dot_product_attention(Q, K, V, is_masked=True):
     d_k = Q.size(-1)
     # first mat mul block on left of diagram
     scores = (Q @ K.mT) / math.sqrt(d_k)
 
     # applies causal mask if we are in the decoder layer
-    if is_encoded:
+    if is_masked:
         seq_len = scores.size(-1)
         causal_mask = torch.triu(torch.ones(seq_len, seq_len, dtype=torch.bool), diagonal=1)
         scores = scores.masked_fill(causal_mask, -1e9)
@@ -32,19 +32,28 @@ class MultiHeadAttention(nn.Module):
         self.W_v = nn.Linear(d_model, d_model,bias=False)
         self.W_o = nn.Linear(d_model, d_model, bias=False)
 
-    def forward(self, x):
+    # in the case of the decoder layer, context is needed from the final output of the encoder stack for attention
+    def forward(self, x, context=None):
         # grab dimensions needed for computation
         batch_size, seq_len, _ = x.size()
 
         # project Q, K, and V
         Q = self.W_q.forward(x)
-        K = self.W_k.forward(x)
-        V = self.W_v.forward(x)
+        if context is not None:
+            # project from the context given
+            K = self.W_k.forward(context)
+            V = self.W_v.forward(context)
+            kv_seq_len = context.size(1)
+        # normal projections
+        else:
+            K = self.W_k.forward(x)
+            V = self.W_v.forward(x)
+            kv_seq_len = seq_len
 
         # split into heads
         Q = Q.view(batch_size, seq_len, self.num_heads, self.d_head).transpose(1, 2)
-        K = K.view(batch_size, seq_len, self.num_heads, self.d_head).transpose(1, 2)
-        V = V.view(batch_size, seq_len, self.num_heads, self.d_head).transpose(1, 2)
+        K = K.view(batch_size, kv_seq_len, self.num_heads, self.d_head).transpose(1, 2)
+        V = V.view(batch_size, kv_seq_len, self.num_heads, self.d_head).transpose(1, 2)
 
         # perform parallel scaled_dot_product_attention across all the heads that Q, K, and V have been split into
         output = scaled_dot_product_attention(Q, K, V, self.is_encoded)
