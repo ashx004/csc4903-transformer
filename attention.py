f1f336f5fdbee0d80cff17ba8df328cc8665d9a3
ashx004 <ashx004@gmail.com>
2026-07-15 19:31:22 -0500

    Implemented multihead attention

--- attention.py
+++ attention.py
@@ -18,3 +18,40 @@ def scaled_dot_product_attention(Q, K, V, is_encoded=True):
     weights = torch.softmax(scores, dim=-1)
     return weights @ V
 
+class MultiHeadAttention(nn.Module):
+    def __init__(self, d_model, num_heads, is_encoded=True):
+        super().__init__()
+        self.d_model = d_model
+        self.num_heads = num_heads
+        self.d_head = d_model // num_heads
+        self.is_encoded = is_encoded
+
+        # weight tensors (not allowing a bias, not in the original paper)
+        self.W_q = nn.Linear(d_model, d_model, bias=False)
+        self.W_k = nn.Linear(d_model, d_model, bias=False)
+        self.W_v = nn.Linear(d_model, d_model,bias=False)
+        self.W_o = nn.Linear(d_model, d_model, bias=False)
+
+    def forward(self, x):
+        # grab dimensions needed for computation
+        batch_size, seq_len, _ = x.size()
+
+        # project Q, K, and V
+        Q = self.W_q.forward(x)
+        K = self.W_k.forward(x)
+        V = self.W_v.forward(x)
+
+        # split into heads
+        Q = Q.view(batch_size, seq_len, self.num_heads, self.d_head).transpose(1, 2)
+        K = K.view(batch_size, seq_len, self.num_heads, self.d_head).transpose(1, 2)
+        V = V.view(batch_size, seq_len, self.num_heads, self.d_head).transpose(1, 2)
+
+        # perform parallel scaled_dot_product_attention across all the heads that Q, K, and V have been split into
+        output = scaled_dot_product_attention(Q, K, V, self.is_encoded)
+
+        # concatenating all heads back into the original configuration before split
+        output = output.transpose(1, 2).contiguous().view(batch_size, seq_len, self.d_model)
+        
+        output = self.W_o.forward(output)
+
+        return output
